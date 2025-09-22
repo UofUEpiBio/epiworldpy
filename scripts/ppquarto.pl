c54c584b59b60d22d1a22a6140574d72ddde9f94
Olivia Banks <elegant.lion1334@fastmail.com>
2025-09-22 14:34:21 -0600

    [ppquarto] remove svg inlining

--- scripts/ppquarto.pl
+++ scripts/ppquarto.pl
@@ -10,25 +10,25 @@ my $prefix = "https://raw.githubusercontent.com/UofUEpiBio/epiworldpy/refs/heads
 my $html = read_file($infile);
 
 # Inline <img src="*.svg">
-$html =~ s{
-    (<img\b[^>]*?\s+src\s*=\s*["'])(?!https?:|data:|/)([^"']+)(["'])
-}{
-    do {
-        my $file = "docs/$2";
-        print $file . "\n";
-        if (-f $file) {
-            local $/;
-            open my $fh, "<:raw", $file;
-            my $svg = <$fh>;
-            close $fh;
-            my $b64 = encode_base64($svg, "");
-            $1 . "data:image/svg+xml;base64," . $b64 . $3;
-        } else {
-            # fallback: leave the img tag as-is if file not found
-            $&
-        }
-    }
-}egxis;
+#$html =~ s{
+#    (<img\b[^>]*?\s+src\s*=\s*["'])(?!https?:|data:|/)([^"']+)(["'])
+#}{
+#    do {
+#        my $file = "docs/$2";
+#        print $file . "\n";
+#        if (-f $file) {
+#            local $/;
+#            open my $fh, "<:raw", $file;
+#            my $svg = <$fh>;
+#            close $fh;
+#            my $b64 = encode_base64($svg, "");
+#            $1 . "data:image/svg+xml;base64," . $b64 . $3;
+#        } else {
+#            # fallback: leave the img tag as-is if file not found
+#            $&
+#        }
+#    }
+#}egxis;
 
 
 # Prefix all src attributes
